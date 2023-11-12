a80addaf630aabb5018e73314224b98ef5f419a3
T.Bilguun <bilguun.bb94@gmail.com>
2023-11-13 00:45:05 +0800

    added .env file

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { getApps, getApp, initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDn2oYfGBKtIGVqZYW0JnvhGK9T99wM6uo",
-  authDomain: "wordmemorizer-5bbc2.firebaseapp.com",
-  projectId: "wordmemorizer-5bbc2",
-  storageBucket: "wordmemorizer-5bbc2.appspot.com",
-  messagingSenderId: "425923280427",
-  appId: "1:425923280427:web:c6f9cae2c0d157808db46e",
-  measurementId: "G-N38SV9MHRG"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGEING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
