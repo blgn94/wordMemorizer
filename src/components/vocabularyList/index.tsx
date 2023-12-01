77782e44d1de64a435e21bfddc171fa693dc33f3
T.Bilguun <bilguun.bb94@gmail.com>
2023-12-01 17:39:19 +0800

    moved vocabulary section to the component folder

---
+++ src/components/vocabularyList/index.tsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react';
+import axios from '../../apis/axios';
+import CircularProgress from '@mui/material/CircularProgress'
+import css from './style.module.css';
+
+const VocabularyList = ({vocabulary, session} : any) => {
+    const [isSuccessfulDeleted, setIsSuccessfulDeleted] = useState(false);
+    const deleteWordFromDB = (word : any) => {
+        const email = session.data?.user?.email?.replaceAll('.', ',');
+        if(vocabulary != null) {
+            for(var i=0; i<vocabulary.length; i++) {
+                if(vocabulary[i].wordEn === word) {
+                    vocabulary.splice(i, 1);
+                    break;
+                }
+            }
+            axios.put(`/users/${email}/words.json`, vocabulary).then(res => {
+                setIsSuccessfulDeleted(true);
+                console.log('delete', res.data);
+            })
+        }
+    }
+
+    useEffect(() => {
+        setTimeout(() => {setIsSuccessfulDeleted(false)}, 1000);
+    }, [isSuccessfulDeleted])
+
+    return (
+        <div>
+            <div className={css.titleSection}>
+                <h1>Collected words</h1>
+                {isSuccessfulDeleted && <CircularProgress size={30}/>}
+            </div>
+            <div className={css.vocabularySection}>
+                {
+                    (vocabulary !== undefined && vocabulary !== null) ? vocabulary?.map((item : any) => {
+                        return(
+                            <div key={item.wordEn} className={css.wordContainer}>
+                                <div className={css.wordStyle}>
+                                    <span className={css.exactWordStyle}>{`${item.wordEn} (${item.type})`}</span> {" - "} <span className={css.mongolianWordStyle}>{item.definitionMn}</span>
+                                </div>
+                                <button className={css.deleteButton} onClick={() => deleteWordFromDB(item.wordEn)}>X</button>
+                            </div>
+                        );
+                    }) : 
+                    <div className={css.wordContainer}>
+                        <span>Хоосон байна!</span>
+                    </div>
+                }
+            </div>
+        </div>
+    );
+};
+
+export default VocabularyList;
