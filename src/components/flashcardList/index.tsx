753fb561e02f10bc2e5906764f1c0ea7d0f6347f
T.Bilguun <bilguun.bb94@gmail.com>
2023-12-01 17:34:31 +0800

    added flash card list

---
+++ src/components/flashcardList/index.tsx
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+import css from './style.module.css';
+import Flashcard from "../flashcard";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { faCheck} from '@fortawesome/free-solid-svg-icons';
+import { faXmark} from '@fortawesome/free-solid-svg-icons';
+
+const FlashcardList = ({ vocabulary, setWhichMenuClicked, learningCount, setLearningCount} : any) => {
+    const [ whichWord, setWhichWord ] = useState(0);
+    const [isCardFlipped, setIsCardFlipped] = useState(false);
+    const [ knowCount, setKnowCount ] = useState(0);
+
+    const knowButtonHandle = () => {
+        if(vocabulary[0]) {
+            if(whichWord < vocabulary.length - 1) {
+                setWhichWord(whichWord + 1);
+                setKnowCount(knowCount + 1);
+            }
+            else if(whichWord == vocabulary.length - 1) {
+                setKnowCount(knowCount + 1);
+                setWhichMenuClicked('flashcardWinningPage');
+            }
+        }
+    }
+
+    const learningButtonHandle = () => {
+        if(vocabulary[0]) {
+            if(whichWord < vocabulary.length - 1) {
+                setWhichWord(whichWord + 1);
+                setLearningCount(learningCount + 1);
+            }
+            else if(whichWord == vocabulary.length - 1) {
+                setLearningCount(learningCount + 1);
+                setWhichMenuClicked('flashcardWinningPage');
+            }
+        }
+    }
+
+    return (
+        <div>
+            <div className={css.flashcardContainer}>
+                {
+                    (vocabulary !== undefined && vocabulary !== null) ?
+                    <div className={css.scoreSection}>
+                        <p className={css.learningText}>{learningCount} Learning</p>
+                        <p>{whichWord+1}/{vocabulary.length}</p>
+                        <p className={css.knowText}>{knowCount} know</p>
+                    </div> :
+                    <div className={css.scoreSection}>
+                        <p className={css.learningText}>0 Learning</p>
+                        <p>{0}/{0}</p>
+                        <p className={css.knowText}>0 know</p>
+                    </div>
+                }
+                <Flashcard vocabulary={vocabulary} index={whichWord} isCardFlipped={isCardFlipped} setIsCardFlipped={setIsCardFlipped} />
+                {
+                    (vocabulary !== undefined && vocabulary !== null) ?
+                    <div className={css.flashcardButtons}>
+                        <div className={css.faXmark} onClick={() => {learningButtonHandle(); if(isCardFlipped) setIsCardFlipped(false)}}>
+                            <FontAwesomeIcon width={30} icon={faXmark} />
+                        </div>
+                        <div className={css.faCheck} onClick={() => {knowButtonHandle(); if(isCardFlipped) setIsCardFlipped(false)}}>
+                            <FontAwesomeIcon width={30} icon={faCheck} />
+                        </div>
+                    </div> :
+                    <div className={css.flashcardButtons}>
+                        <div className={css.faXmark}>
+                            <FontAwesomeIcon width={30} icon={faXmark} />
+                        </div>
+                        <div className={css.faCheck} >
+                            <FontAwesomeIcon width={30} icon={faCheck} />
+                        </div>
+                    </div>
+                }
+            </div>
+        </div>
+    );
+};
+
+export default FlashcardList;
